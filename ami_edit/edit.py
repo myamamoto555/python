3247a7db90e8be7e8bae93a6c1a3a93bbfe6016e
myamamoto555 <yamamoto@soft.ics.keio.ac.jp>
2016-11-11 17:45:26 +0900

    update

---
+++ ami_edit/edit.py
@@ -0,0 +1,97 @@
+# coding:utf-8
+import os
+import json
+
+# make filelist
+files = os.listdir('words/')
+filelist = []
+for f in files:
+    tmpfile = f.split('.')[0]
+    if tmpfile not in filelist:
+        filelist.append(tmpfile+'.')
+
+person = ["A", "B", "C", "D"]
+output_dir = 'plain/'
+
+
+def decode(filename, p, start_word, end_word):
+    sentence = ''
+    flag = 0
+    f = open('words/' + filename + p + '.words.xml')
+    for l in f:
+        if start_word in l or (flag == 1 and end_word not in l):
+            w = l.split('>')[1].split('<')[0]
+            if w!= '\n':
+                sentence += w + ' '
+            else:
+                sentence = sentence[:-1]
+            flag = 1
+            if end_word == 'none':
+                break
+        if end_word in l:
+            w = l.split('>')[1].split('<')[0]
+            if w != '\n':
+                sentence += w
+            else:
+                sentence = sentence[:-1]
+            flag = 0
+            break
+    sentence = sentence.replace("&#39;", "'")
+    return sentence
+
+
+def get_topic_segment(filename):
+    topic_segment = []
+    flag = 0
+    try:
+        with open('topics/' + filename + 'topic.xml') as f:
+            for l in f:
+                if '#id' in l:
+                    tmp_segment = l.split('#id(')[1].split(')')[0]
+                if flag == 1:
+                    topic_segment.append(tmp_segment)
+                    flag = 0
+                if 'nite:pointer' in l:
+                    flag = 1
+    except:
+        topic_segment = ['*']
+    return topic_segment
+
+
+fout = open('ami_plain.jsonl', 'w')
+
+for filename in filelist:
+    topic_segment = get_topic_segment(filename)
+    timedic = {}
+    for p in person:
+        with open('segments/' + filename + p + '.segments.xml') as f:
+            for l in f:
+                if "transcriber_start" in l:
+                    start_time = l.split('transcriber_start="')[1].split('"')[0]
+                    end_time = l.split('transcriber_end="')[1].split('"')[0]
+                if "nite:child" in l:
+                    start_word = l.split('#id(')[1].split(')')[0]
+                    try:
+                        end_word = l.split('..id(')[1].split(')')[0]
+                    except:
+                        end_word = 'none'
+                    sentence = decode(filename, p, start_word, end_word)
+                    if sentence != '':
+                        if topic_segment == ['*']:
+                            timedic[float(start_time)] = [p, sentence, '*']
+                        else:
+                            if start_word in topic_segment:
+                                timedic[float(start_time)] = [p, sentence, '1']
+                            else:
+                                timedic[float(start_time)] = [p, sentence, '0']
+
+    for k, v in sorted(timedic.items()):
+        infodic = {}
+        infodic['id'] = filename
+        infodic['speaker'] = v[0]
+        infodic['text'] = v[1]
+        infodic['segment'] = v[2]
+        
+        jl = json.dumps(infodic, ensure_ascii=False)
+        fout.write(jl + '\n')
+fout.close()
