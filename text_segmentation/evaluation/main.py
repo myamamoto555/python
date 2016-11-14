7c3732e70977c3336cbbed9ada7aa7ce49c1d7ca
myamamoto555 <yamamoto@soft.ics.keio.ac.jp>
2016-11-14 16:31:46 +0900

    update evaluation

--- text_segmentation/evaluation/main.py
+++ text_segmentation/evaluation/main.py
@@ -1,2 +1,30 @@
 #coding:utf-8
 
+class evaluation(object):
+    def __init__(self, predict_file, answer_file):
+        self.predict = []
+        self.answer = []
+        self.datastore(predict_file, 'predict')
+        self.datastore(answer_file, 'answer')
+    
+    def datastore(self, filename, listname):
+        if listname == 'predict':
+            lit = self.predict
+        else:
+            lit = self.answer
+
+        with open(filename) as f:
+            list_tmp = []
+            for l in f:
+                flag = l.split('\n')[0]
+                if flag == '1' or flag == '0':
+                    list_tmp.append(flag)    
+                if flag == '-1':
+                    lit.append(list_tmp)
+                    list_tmp = []
+
+
+if __name__ == '__main__':
+    ev = evaluation('example.predict', 'example.answer')
+    print ev.predict
+    print ev.answer
