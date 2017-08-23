4d546dcccf4d47284db2bbd290c24e08dd6d59ac
myamamoto555 <yamamoto@soft.ics.keio.ac.jp>
2017-08-23 14:39:38 +0900

    cleanup

--- translation/my_nmt/testcodes/get_top_k.py
+++ translation/my_nmt/testcodes/get_top_k.py
@@ -22,3 +22,22 @@ x = np.array([[1,2,3],[4,5,6]])
 print x
 print top_k(x, 2)
 print np.zeros(5)
+
+
+def top_k_batch(xs, k, b):
+    ids_list = []
+    scores_list = []
+    for b in range(b):
+        x = xs[k*b:k*(b+1)]
+        for i in range(k):
+            ids = np.unravel_index(x.argmax(), x.shape)
+            score = x[ids[0]][ids[1]]
+            ids_list.append((ids[0]+k*b,ids[1]))
+            scores_list.append(score)
+            x[ids[0]][ids[1]] = -1000000
+    return ids_list, scores_list
+
+
+x = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+print x
+print top_k_batch(x, 1, 4)
